21b504572ad9dbbdcb3d6481baca2f7e77cfc000
Nico-Serra <nico.serra000@gmail.com>
2024-02-27 17:03:34 +0100

    add loop while

--- pari e dispari/assets/js/main.js
+++ pari e dispari/assets/js/main.js
@@ -8,8 +8,33 @@ L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
 */
-let userChoice = confirm('Per poter vincere vuoi che il tuo numero sia pari?')
-//console.log(userChoice);
+let even = 'pari'
+let odd = 'dispari'
+//let userChoice = prompt('Scrivi pari o dispari')
+//console.log(userChoice !== even && userChoice !== odd);
+
+let userChoice = prompt('Scrivi pari o dispari')
+
+let wrong = userChoice === even || userChoice === odd
+console.log(wrong);
+  while (wrong === false) {
+    userChoice = prompt('Riscrivi pari o dispari')
+    
+ }
+
+console.log(userChoice);
+
+// if (userChoice !== even && userChoice !== odd);
+// {
+//     alert('il tuo valore inserito è sbagliato prova a riscirverlo')
+// }
+
+// if (userChoice !== even) {
+//     userChoice = prompt('reinserisci pari o dispari')
+// }
+
+//let userChoice = confirm('Per poter vincere vuoi che il tuo numero sia pari?')
+
 
 let userNumber = Number(prompt('Inserisci un numero da 1 a 5'))
 //console.log(userNumber);
@@ -21,14 +46,14 @@ let userNumber = Number(prompt('Inserisci un numero da 1 a 5'))
  * @param {number} max 
  */
 function makeRandomNumber(min, max) {
-    return Math.floor(Math.random()*5)+1
+    return Math.floor(Math.random() * max) + min
 }
 
-const randomNumber = makeRandomNumber(1,5)
+const randomNumber = makeRandomNumber(1, 5)
 
 //console.log(randomNumber);
 
-const pcNumber = makeRandomNumber(1,5)
+const pcNumber = makeRandomNumber(1, 5)
 
 //console.log(pcNumber);
 
@@ -53,10 +78,10 @@ const sumPc = pcNumber + randomNumber
  */
 function sumIsEven(sumUser) {
     if (userChoice === true && sumUser % 2 === 0) {
-     return alert(`Complimenti hai vinto il tuo numero è pari ${sumUser}`)
- } else if (userChoice === false && sumUser % 2 === 1) {
-    return alert(`Complimenti hai vinto il tuo numero è dispari ${sumUser}`)
- } else { return alert(`Mi dispiace hai perso il tuo numero non è cio che hai scelto`)}
+        return alert(`Complimenti hai vinto il tuo numero è pari ${sumUser}`)
+    } else if (userChoice === false && sumUser % 2 === 1) {
+        return alert(`Complimenti hai vinto il tuo numero è dispari ${sumUser}`)
+    } else { return alert(`Mi dispiace hai perso il tuo numero non è cio che hai scelto`) }
 }
 
 sumIsEven(sumUser)
